b915ce483ff983c057212b6ce6f1a2846b2232f5
Yichu <yichu.li@outlook.com>
2024-03-25 16:06:39 -0400

    functions of comments and post, tested by postman

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
-
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
